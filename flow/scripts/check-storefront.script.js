f096c6fe159a29e87d5256a535344dc2dc17d3a5
Leon Acosta <leon@dandelionlabs.io>
2021-11-29 14:34:46 +0700

    Reorganized the repository

--- flow/scripts/check-storefront.script.js
+++
@@ -1,9 +0,0 @@
-export const CHECK_STOREFRONT = `
-  import LimitdNFTStorefront from 0xLimitd
-
-   pub fun main(_ address: Address): Bool {
-    return getAccount(address)
-      .getCapability<&LimitdNFTStorefront.Storefront{LimitdNFTStorefront.StorefrontPublic}>(LimitdNFTStorefront.StorefrontPublicPath)
-      .check()
-  }
-  `;
